485511ec82d1144ab2356af6a37b3eb0165d38b1
Wipple <krugthebearking@gmail.com>
2023-05-15 15:00:19 -0400

    init commit

---
+++ src/pages/test.tsx
@@ -0,0 +1,29 @@
+const TestPage = () => {
+    //use the useAxios hook and pass the AxiosRequestConfig
+    // var isLoading: boolean = false
+    // var errorMsg: AxiosError<any, any> | null = null
+    // var responseData: any = null
+  
+    // const makeRequest = (url: string) => {
+    //   const [{ data, loading, error }] = useAxios<any>({
+    //     baseURL: spaceTradersApiUrl,
+    //     url: "v2/register",
+    //   });
+    //   isLoading = loading
+    //   errorMsg = error
+    //   responseData = data
+    // }
+  
+    
+  
+    return (
+      <div>
+        <h1>Test Page</h1>
+        <p>test</p>
+        <div className="@button" ><p>Some button</p></div>
+      </div>
+    );
+  }
+  
+  export default TestPage
+  
